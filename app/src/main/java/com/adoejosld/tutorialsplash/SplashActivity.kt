e737dd31d42a0f7d3a73468fab4edd1107ff744b
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-14 00:09:58 -0400

    add .

--- app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
+++ app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
@@ -1,17 +1,14 @@
 package com.adoejosld.tutorialsplash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         startActivity(Intent(this, MainActivity::class.java))
 
-        }
     }
+}
