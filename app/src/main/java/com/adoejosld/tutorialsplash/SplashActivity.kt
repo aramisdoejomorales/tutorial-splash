c8bd35a9ff37cb8cc7a2713a443ddd4d8f1a64d8
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-14 00:17:39 -0400

    add .

--- app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
+++ app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
