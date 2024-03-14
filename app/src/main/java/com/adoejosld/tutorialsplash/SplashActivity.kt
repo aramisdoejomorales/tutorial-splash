2a0c805d40b1ccf642f2cd1d65992469cdddf6a6
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-13 23:40:59 -0400

    add .

--- app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
+++ app/src/main/java/com/adoejosld/tutorialsplash/SplashActivity.kt
@@ -12,10 +12,6 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         startActivity(Intent(this, MainActivity::class.java))
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
         }
     }
-}
